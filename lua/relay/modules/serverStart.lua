d4405ff361a6268fbc482f8ed297ef4dd185f187
Marshall_vak <Marshall_vak@outlook.com>
2023-03-10 08:21:38 -0500

    rename file

--- lua/relay/modules/serverStart.lua
+++ lua/relay/modules/serverStart.lua
@@ -4,6 +4,9 @@ hook.Add("InitPostEntity", "DS_Discord", function()
         ["avatar_url"] = Discord.avatar,
         ["embeds"] = {{
             ["title"] = "Server is now online!",
+            ["thumbnail"] = {
+				["url"] = "https://cdn.dservers.xyz/images/map-icons/" .. game.GetMap() .. ".png",
+			},
             ["description"] = "Map: " .. game.GetMap() .. "\nJoin now at steam://connect/" .. game.GetIPAddress(),
             ["color"] = Discord.color
         }}
